da4c7ed1442d8b706a5ab4f1769cfddd34066e70
Justin Weiss <justin@uberweiss.org>
2011-09-01 23:16:55 -0700

    Added /tmp and /public directories for passenger

--- LogServer/lib/log_server.rb
+++ LogServer/lib/log_server.rb
@@ -7,11 +7,13 @@ class LogServer < Sinatra::Base
   end
   
   post '/log' do
-    log_dir = Pathname.new(File.expand_path('log', File.dirname(__FILE__)))
-    filename = "#{Time.now.strftime("%F-%H%M%S")}-#{params[:udid]}.log"
-    
-    File.open(log_dir + filename, 'w') do |f|
-      f.puts params[:body]
+    if params[:udid]
+      log_dir = Pathname.new(File.expand_path('log', File.dirname(__FILE__)))
+      filename = "#{Time.now.strftime("%F-%H%M%S")}-#{params[:udid]}.log"
+      
+      File.open(log_dir + filename, 'w') do |f|
+        f.puts params[:body]
+      end
     end
   end
 end
