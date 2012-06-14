0adaf761d8e1da005eaaf9e53f520d62ea9ea6f5
Sef Kloninger <sef@kloninger.com>
2012-06-14 01:19:49 -0700

    Add --sudo option.
    Run all commands under sudo.  If selected, prints a little warning
    saying that they may be prompted for their password, and then prefix all
    commands with sudo.  Useful for people (like me) that don't enable
    configuration things like brew and gem straight from my account.
    
    Not doing any real option parsing, just checking $1 for that flag.
    Could be made smarter.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
+if [[ "$1" == "--sudo" ]]; then
+    echo "All commands (brew, gem, pod) will be run with sudo. You may be prompted by sudo for your password."
+    CMD_PREFIX="sudo"
+fi
+
 if [[ -x `which brew` && ! -d ~/Library/Application\ Support/appledoc ]]; then
-    brew install appledoc --HEAD
-    ln -sf "`brew --prefix`/Cellar/appledoc/HEAD/Templates" ~/Library/Application\ Support/appledoc
+    ${CMD_PREFIX} brew install appledoc --HEAD
+    ${CMD_PREFIX} ln -sf "`brew --prefix`/Cellar/appledoc/HEAD/Templates" ~/Library/Application\ Support/appledoc
 fi
 
 if [ ! -d DGSPhone.xcworkspace ]; then
-    gem install cocoapods
-    pod setup
-    pod install DGSPhone.xcodeproj
+    ${CMD_PREFIX} gem install cocoapods
+    ${CMD_PREFIX} pod setup
+    ${CMD_PREFIX} pod install DGSPhone.xcodeproj
 else
-    pod install
+    ${CMD_PREFIX} pod install
 fi
